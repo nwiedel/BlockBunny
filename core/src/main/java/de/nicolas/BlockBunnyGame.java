a77fa0ae2ff8477d7a0118dc829eead1bbc0a5c4
nicolaswiedel <nicolaswiedel@googlemail.com>
2026-05-26 14:28:10 +0200

    1.2. Video 1 Ende

--- core/src/main/java/de/nicolas/BlockBunnyGame.java
+++ core/src/main/java/de/nicolas/BlockBunnyGame.java
@@ -2,28 +2,47 @@ package de.nicolas;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 import de.nicolas.config.GameConfig;
+import de.nicolas.handlers.GameStateManager;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class BlockBunnyGame implements ApplicationListener {
+
+    private static final float STEP = 1 / 60f;
+    private float accum;
 
     private SpriteBatch batch;
     private OrthographicCamera camera;
     private OrthographicCamera hudCamera;
 
+    private GameStateManager gsm;
+
     @Override
     public void create() {
         batch = new SpriteBatch();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, GameConfig.V_WIDTH, GameConfig.V_HEIGHT);
+        hudCamera = new OrthographicCamera();
+        hudCamera.setToOrtho(false, GameConfig.V_WIDTH, GameConfig.V_HEIGHT);
+
+        gsm = new GameStateManager(this);
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(GameConfig.CORNFLOWER_BLUE);
 
+        accum += Gdx.graphics.getDeltaTime();
+        while (accum >= STEP){
+            accum -= STEP;
+            gsm.update(STEP);
+            gsm.render();
+        }
     }
 
     /** Getter und Setter */
--- core/src/main/java/de/nicolas/states/Play.java
+++ core/src/main/java/de/nicolas/states/Play.java
@@ -1,8 +1,11 @@
 package de.nicolas.states;
 
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import de.nicolas.handlers.GameStateManager;
 
 public class Play extends GameState {
+
+    private BitmapFont font = new BitmapFont();
 
     public Play(GameStateManager gsm){
         super(gsm);
@@ -20,7 +23,10 @@ public class Play extends GameState {
 
     @Override
     public void render() {
-
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        font.draw(batch, "play stat", 100, 100);
+        batch.end();
     }
 
     @Override
