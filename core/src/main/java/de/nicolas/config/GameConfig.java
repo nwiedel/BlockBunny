fddf7fad88e8cac400bb773a8774acbe3a6ae1d9
nicolaswiedel <nicolaswiedel@googlemail.com>
2026-05-27 14:52:04 +0200

    5.2. Video 5 Ende

--- core/src/main/java/de/nicolas/config/GameConfig.java
+++ core/src/main/java/de/nicolas/config/GameConfig.java
@@ -16,7 +16,10 @@ public class GameConfig {
     public static final float PPM = 100;
 
     // category bits
-    public static final short BIT_GROUND = 2;
-    public static final short BIT_PLAYER = 4;
+    public static final short BIT_PLAYER = 2;
+    public static final short BIT_RED = 4;
+    public static final short BIT_GREEN = 8;
+    public static final short BIT_BLUE = 16;
+
 
 }
--- core/src/main/java/de/nicolas/states/Play.java
+++ core/src/main/java/de/nicolas/states/Play.java
@@ -1,14 +1,19 @@
 package de.nicolas.states;
 
-import static de.nicolas.config.GameConfig.PPM;
-
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 import de.nicolas.config.GameConfig;
 import de.nicolas.handlers.GameStateManager;
 import de.nicolas.handlers.MyContactListener;
 import de.nicolas.handlers.MyInput;
+
+import static de.nicolas.config.GameConfig.PPM;
 
 public class Play extends GameState {
 
@@ -20,6 +25,10 @@ public class Play extends GameState {
     private Body playerBody;
     private MyContactListener contactListener;
 
+    private TiledMap tiledMap;
+    private float tileSize;
+    private OrthogonalTiledMapRenderer mapRenderer;
+
     public Play(GameStateManager gsm){
         super(gsm);
 
@@ -28,22 +37,13 @@ public class Play extends GameState {
         contactListener = new MyContactListener();
         world.setContactListener(contactListener);
 
+       /////// Box2D Kram ///////
+
         debugRenderer = new Box2DDebugRenderer();
 
-        // Platform erstellen
         BodyDef bdef = new BodyDef();
-        bdef.position.set(160 / PPM, 120 / PPM);
-        bdef.type = BodyDef.BodyType.StaticBody;
-        Body body = world.createBody(bdef);
-
-        PolygonShape shape = new PolygonShape();
-        shape.setAsBox(50 / PPM, 5 / PPM);
-
         FixtureDef fdef = new FixtureDef();
-        fdef.shape = shape;
-        fdef.filter.categoryBits = GameConfig.BIT_GROUND;
-        fdef.filter.maskBits = GameConfig.BIT_PLAYER;
-        body.createFixture(fdef).setUserData("ground");
+        PolygonShape shape = new PolygonShape();
 
         // fallender Player
         bdef.position.set(160 / PPM, 200 / PPM);
@@ -54,14 +54,14 @@ public class Play extends GameState {
 
         fdef.shape = shape;
         fdef.filter.categoryBits = GameConfig.BIT_PLAYER;
-        fdef.filter.maskBits = GameConfig.BIT_GROUND;
+        fdef.filter.maskBits = GameConfig.BIT_RED;
         playerBody.createFixture(fdef).setUserData("player");
 
         // kreiere foot sensor
         shape.setAsBox(2 / PPM, 2 / PPM, new Vector2(0, -5 / PPM),0);
         fdef.shape = shape;
         fdef.filter.categoryBits = GameConfig.BIT_PLAYER;
-        fdef.filter.maskBits = GameConfig.BIT_GROUND;
+        fdef.filter.maskBits = GameConfig.BIT_RED;
         fdef.isSensor = true;
         playerBody.createFixture(fdef).setUserData("foot");
 
@@ -69,6 +69,47 @@ public class Play extends GameState {
         b2dCamera = new OrthographicCamera();
         b2dCamera.setToOrtho(false,
             GameConfig.V_WIDTH / PPM, GameConfig.V_HEIGHT / PPM);
+
+        ////////////////////////
+
+        // Map laden
+        tiledMap = new TmxMapLoader().load("maps/test.tmx");
+        mapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+
+        TiledMapTileLayer layer = (TiledMapTileLayer) tiledMap.getLayers().get("red");
+
+        tileSize = layer.getTileWidth();
+
+        for (int row = 0; row < layer.getHeight(); row++){
+            for (int col = 0; col < layer.getWidth(); col++){
+                // Zelle holen
+                Cell cell = layer.getCell(col, row);
+
+                // Existenz checken
+                if (cell == null) continue;
+                if (cell.getTile() == null) continue;
+
+                // Box2D Body und Fixture erstellen
+                bdef.type = BodyDef.BodyType.StaticBody;
+                bdef.position.set(
+                    (col + 0.5f) * tileSize / PPM,
+                    (row + 0.5f) * tileSize / PPM
+                );
+
+                ChainShape cs = new ChainShape();
+                Vector2[] v = new Vector2[3];
+                v[0] = new Vector2(-tileSize / 2 / PPM, -tileSize / 2 / PPM);
+                v[1] = new Vector2(-tileSize / 2 / PPM, tileSize / 2 / PPM);
+                v[2] = new Vector2(tileSize / 2 / PPM, tileSize  / 2 / PPM);
+                cs.createChain(v);
+                fdef.friction = 0;
+                fdef.shape = cs;
+                fdef.filter.categoryBits = GameConfig.BIT_RED;
+                fdef.filter.maskBits = GameConfig.BIT_PLAYER;
+                fdef.isSensor = false;
+                world.createBody(bdef).createFixture(fdef);
+            }
+        }
     }
 
     @Override
@@ -91,6 +132,11 @@ public class Play extends GameState {
 
     @Override
     public void render() {
+
+        // Map zeichnen
+        mapRenderer.setView(camera);
+        mapRenderer.render();
+
         debugRenderer.render(world, b2dCamera.combined);
     }
 
